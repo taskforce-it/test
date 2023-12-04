0a3c03997cd1ef7a7c6c6b62fef2077edf58a731
Frank Hildebrandt <fhildebrandt@taskforce-it.de>
2023-12-04 14:28:26 +0100

    Fritz...

--- Rapid Fire Build/src/biz/rapidfire/build/Empty.java
+++ Rapid Fire Build/src/biz/rapidfire/build/Empty.java
@@ -5,7 +5,7 @@ public class Empty {
 	// Änderung 1
 	// Änderung 2
 	// Änderung 3
-	// Änderung 4
+	// Änderung 4 Frank Heino
 	// Änderung 5 Frank Heino
 	// Änderung 6 Frank Heino
 	// Änderung 7 Frank Heino
